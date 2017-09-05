d81fb0c04ba0891db3aabe3f3470974acda5fa29
Curtis H <CurtLHampton@gmail.com>
2017-09-04 22:18:18 -0500

    Add code to get current weather and forecast

---
+++ wq/get_weather_forecast.py
@@ -0,0 +1,41 @@
+from wq import wunderground as wg
+import psycopg2
+from datetime import datetime
+import json
+from time import sleep
+
+# connect to the databse
+conn = psycopg2.connect(database="postgres",
+                        user="postgres",
+                        password="apassword",
+                        host="192.168.0.105",
+                        port="5432")
+
+# enable autocommit
+conn.autocommit = True
+
+# define cursor
+cur = conn.cursor()
+
+# create a table
+cur.execute("""CREATE TABLE IF NOT EXISTS arlington_weather_forecast
+               (id SERIAL PRIMARY KEY NOT NULL,
+                datetime timestamp NOT NULL,
+                forecast jsonb NOT NULL)""")
+
+# load API key
+key = wg.load_key("/home/curtis/etc/wunderground")
+
+# build API request
+url = wg.build_request(key, database='forecast', city='Arlington', state='VA')
+
+# get results
+r = wg.get_request(url) 
+
+# get current datetime
+dt_now = datetime.now()
+
+# put reading into databse
+cur.execute("""INSERT INTO arlington_weather_forecast
+               (datetime, forecast) 
+               VALUES (%s, %s)""", [dt_now, json.dumps(r)])
