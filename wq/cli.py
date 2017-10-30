b390bc9c4612fe01aabb1693962708aeccf0ac30
Curtis H <CurtLHampton@gmail.com>
2017-10-29 22:29:21 -0400

    Set default database as conditions

--- wq/cli.py
+++ wq/cli.py
@@ -3,7 +3,7 @@ from wq import wunderground as wg
 from pprint import pprint
 
 @click.command()
-@click.option('--database', type=click.Choice(wg.databases()))
+@click.option('--database', type=click.Choice(wg.databases()), default='conditions')
 @click.argument('city')
 @click.argument('state')
 def cli(database, city, state):
